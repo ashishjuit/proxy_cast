2d85d430d92c090b6b9a8e0fd4962c15527940b9
ASHISH AGGARWAL <ashishjyc@gmail.com>
2016-07-20 19:07:26 -0400

    added the for loop to retrive specific data from daily

--- index.js
+++ index.js
@@ -34,14 +34,13 @@ server.get('/forecast/hourly/:lat,:lon', function(req, res){
   });
 });
 
-
-server.get('/forecast/minutely/:lat,:lon', function(req, res){
-  $http.get(baseUrl + apiKey + '/'+req.params.lat + ',' + req.params.lon)
+server.get('/forecast/daily/:lat,:lon', function(req, res){
+  $http.get(baseUrl + apiKey + '/'+req.params.lat+','+req.params.lon)
   .then(function(response){
     var resObj = {
       latitude:response.data.latitude,
       longitude: response.data.longitude,
-      minutely: response.data.minutely,
+      daily: response.data.daily,
     }
     res.status(200).json(resObj);
   })
@@ -52,6 +51,60 @@ server.get('/forecast/minutely/:lat,:lon', function(req, res){
     })
   });
 });
+
+
+// server.get('/forecast/minutely/:lat,:lon', function(req, res){
+//   $http.get(baseUrl + apiKey + '/'+req.params.lat + ',' + req.params.lon)
+//   .then(function(response){
+//     var resObj = {
+//       latitude:response.data.latitude,
+//       longitude: response.data.longitude,
+//       minutely: response.data.minutely,
+//     }
+//     res.status(200).json(resObj);
+//   })
+//   .catch(function(err){
+//     console.log(err);
+//     res.status(500).send({
+//       msg: error
+//     })
+//   });
+// });
+
+server.get('/forecast/minutely/:lat,:lon', function(req, res){
+  $http.get(baseUrl + apiKey + '/' + req.params.lat + ','+ req.params.lon)
+  .then(function(response){
+    var overSummary = response.data.daily.summary;
+    var overIcon = response.data.daily.icon;
+    var dailyData= response.data.daily.data;
+    var dailyArr = [];
+    for(var i=0; i<dailyData.length; i+=1){
+      var o = {
+        icon: dailyData[i].icon,
+        tempMax: dailyData[i].temperatureMax,
+        tempMin: dailyData[i].temperatureMin,
+        humidity: dailyData[i].humidity,
+        precipProb: dailyData[i].precipProbability
+      };
+      dailyArr.push(o);
+    }
+
+    var resObj = {
+      latitude: response.data.daily.lattitude,
+      longitude: response.data.longitude,
+      summary: overSummary,
+      icon: overIcon,
+      daily: dailyArr
+    };
+    res.status(200).json(resObj);
+  })
+  .catch(function(err){
+    res.status(500).json({
+        msg: err
+    });
+  });
+});
+
 
 //listen
 server.listen(port, function(){
